0c96f16e418471eb214eaf095fc7bd69cefd172c
Siddhesh <siddheshwayal114@gmail.com>
2024-02-10 17:48:47 +0530

    hero section done

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from '../images/Logo.png'
 
 function Herosection() {
   return (
     <div className="Herosection flex flex-row items-center justify-around mt-80   text-white">
       <div
-        className="flex flex-col py-3"
+        className="flex flex-col py-3 justify-center"
         style={{ width: "531px", height: "383px" }}
       >
         <div>
@@ -16,19 +17,26 @@ function Herosection() {
           </h1>
         </div>
         <div style={{ fontSize: "18px", lineHeight: "22px" }}>
-        <h4 className="font-normal mt-2 font-Archivo">   
-          ABIT is a dynamic departmental committee that provides a platform for
-          IT students to develop technical and soft skills, enabling them to
-                      excel in their chosen fields.
-                      </h4>    
+          <h4 className="font-normal mt-2 font-Archivo">
+            ABIT is a dynamic departmental committee that provides a platform
+            for IT students to develop technical and soft skills, enabling them
+            to excel in their chosen fields.
+          </h4>
         </div>
-        <button>Hey</button>
+        <div>
+          <button
+            className="my-5 w-36 h-9 rounded-3xl"
+            style={{ backgroundColor: "#009EFF" }}
+          >
+            <p className="font-normal font-Archivo text-lg"> Learn More </p>
+          </button>
+        </div>
       </div>
       <div
         className="flex items-center justify-center"
         style={{ width: "531px", height: "383px" }}
       >
-        Block2
+        <img src={Logo} alt="" />
       </div>
     </div>
   );
