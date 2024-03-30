373edee01a3b1b6de84123419c7cd41a96491c3a
Sahil R <sahilrw000@gmail.com>
2024-03-30 18:31:10 +0530

    footer and event pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,101 +6,68 @@ import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex flex-col items-center">
-        <div className="flex flex-col lg:flex-row mb-4 lg:mb-0 lg:w-3/4 lg:mr-16">
-          <img src={logofooter} alt="" />
-          <div className='flex flex-col ml-10'>
-            <h2 className="text-lg font-semibold mb-2 text-center lg:text-left ml-4">Location :</h2>
-            <p className="text-gray-300 w-[250px] lg:w-2/3 text-center mb-8">
-              MCT's Rajiv Gandhi Institute Of Technology, Juhu Versova Link Rd,
-              Versova, Andheri West, Mumbai-400053
-            </p>
+    <footer className="bg-gray-800 text-white py-8 md:grid md:gap-8">
+      <section className='md:flex md:w-[1068px] md:h-[195px] mx-[150px]'>
+        <section className="md:w-[746px] md:h-[200px] mx-auto flex flex-col items-center md:items-start md:gap-8">
+          <div className="flex flex-col md:flex-row mb-4 md:mb-0">
+            <img src={logofooter} alt="" className='md:w-[232px] md:h-[92px]' />
+            <div className='flex flex-col ml-10'>
+              <h2 className="text-lg font-medium mb-2 md:font-normal md:mb-0 text-center lg:text-left">Location :</h2>
+              <p className="text-gray-300 w-[250px] md:w-[380px] md:h-[70px] md:text-xl text-center md:text-left mb-8">
+                MCT's Rajiv Gandhi Institute Of Technology, Juhu Versova Link Rd,
+                Versova, Andheri West, Mumbai-400053
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 mb-6">
-          <div>
-            <p className="text-lg font-semibold text-center">Contact : </p>
-            <a
-              href="mailto:abitrgit.it@gmail.com"
-              className="text-gray-300 hover:text-white"
-            >
-              abitrgit.it@gmail.com
-            </a>
-          </div>
-          <div className='flex justify-between '>
-            <FaFacebookSquare size={28} />
-            <FaInstagram size={28} />
-            <FaLinkedin size={28} />
-          </div>
-        </div>
-        <div className="flex flex-col text-center gap-3">
-          <div className='flex flex-col'>
+          <section className="flex flex-col md:flex-row gap-4 mb-6 text-center md:text-left md:w-[746px] md:h-[71px]">
+            <div className='md:ml-4'>
+              <p className="text-lg font-medium md:font-normal">Contact : </p>
+              <a
+                href="mailto:abitrgit.it@gmail.com"
+                className="text-gray-300 hover:text-white md:text-xl"
+              >
+                abitrgit.it@gmail.com
+              </a>
+            </div>
+            <div className='flex justify-between md:w-[200px] md:ml-16'>
+              <FaFacebookSquare size={28} />
+              <FaInstagram size={28} />
+              <FaLinkedin size={28} />
+            </div>
+          </section>
+        </section>
+        <section className="flex flex-col md:flex-row text-center md:text-left md:text-lg gap-20">
+          <div className='flex flex-col gap-3'>
             <a href="#">About Us</a>
             <a href="#">Faculty</a>
             <a href="#">Team</a>
             <a href="#">Attendance</a>
           </div>
-          <div className='flex flex-col'>
+          <div className='flex flex-col gap-3'>
             <a href="#">Events</a>
             <a href="#">Contact</a>
             <a href="#">Magazine</a>
           </div>
-          {/* <ul className="flex flex-col space-y-2">
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Faculty
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Events
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Team
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Magazine
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Attendance
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Contact
-              </a>
-            </li>
-          </ul> */}
-        </div>
+        </section>
+      </section>
 
-      </div>
-      <hr className="h-px my-8 mx-20 bg-gray-200 border-1 dark:bg-gray-700" />
 
-      <div className="flex justify-between items-center mt-8 ml-20 mr-20 text-gray-300">
-        <div>
-          © 2023 ABITRGIT. All rights reserved.
+      <section className='md:w-[1300px] mx-auto'>
+        <hr className="h-px my-8 mx-20 bg-gray-200 border-1 dark:bg-gray-700" />
+        <div className="flex justify-between items-center mt-8 ml-20 mr-20 text-gray-300">
+          <div>
+            © 2023 ABITRGIT. All rights reserved.
+          </div>
+          <div className="space-x-4">
+            <a href="#" className="hover:text-white">
+              Privacy Policy
+            </a>
+            <a href="#" className="hover:text-white">
+              Terms of Service
+            </a>
+          </div>
         </div>
-        <div className="space-x-4">
-          <a href="#" className="hover:text-white">
-            Privacy Policy
-          </a>
-          <a href="#" className="hover:text-white">
-            Terms of Service
-          </a>
-        </div>
-      </div>
+      </section>
 
     </footer>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,40 @@
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
 import Navbar from "../components/Navbar";
 import Herosection from "../components/Herosection";
 import Coreteam from "../components/Coreteam"
 import Ourmissons from "../components/Ourmissons"
 import Motivators from '../components/Motivators'
 import Potentials from "../components/Potentials"
+import Sponsor from "../components/Sponsor"
+import Events from '../components/Events';
+import Footer from '../components/Footer';
 
 function Home() {
+  useEffect(() => {
+    // Define animation timeline
+    const tl = gsap.timeline();
+
+    // Set initial state
+    tl.set('.component', { opacity: 0, scale: 0.8, filter: 'blur(8px)' });
+
+    // Animation sequence
+    tl.to('.component', { duration: 1, opacity: 1, scale: 1, filter: 'blur(0px)' });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Run only once on component mount
 
   return (
     <>
-   
-          <Navbar />
-          <Herosection />
-          <Coreteam />
-          <Ourmissons />
-          <Motivators />
-          <Potentials/>
-         
+      <Navbar className="component" />
+      <Herosection className="component" />
+      <Coreteam className="component" />
+      <Ourmissons className="component" />
+      <Motivators className="component" />
+      <Potentials className="component" />
+      <Sponsor className="component" />
+      <Events className="component" />
+      <Footer className="component" />
     </>
   );
 }
