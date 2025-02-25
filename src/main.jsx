9a864a4b0cd8bec156e65da16b8adb65435263a6
Uday Bhangare <ubhangare32@gmail.com>
2025-02-25 19:28:56 +0530

    !st Stage

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="overflow-hidden">
-      <img 
-        className="bluespot 
-          sm:h-[150vh] sm:w-full sm:object-cover sm:scale-[3] sm:relative 
-          md:h-[700px] md:w-[1100px] md:ml-[-100px] md:mt-10 md:scale-[1.2] md:fixed 
-          lg:h-[800px] lg:w-[1200px] lg:ml-[400px] lg:mt-20 lg:scale-100 lg:fixed 
-          xl:ml-[500px]"
-        src="spotlightBg.png"
-      />
+    <div>
+      <img className='bluespot sm:ml-0 sm:mt-0 lg:ml-[265px] lg:mt[0px] md:h-[1000px] md:w-[1600px] scale-[1.5] md:scale-[1]' src="spotlightBg.png" />
     </div>
-
     <App />
   </React.StrictMode>,
 )
